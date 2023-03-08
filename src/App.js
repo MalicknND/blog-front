52947de7dc005b844ccceddb36583ba8b67bb545
Malick NDIAYE <98133571+MalicknND@users.noreply.github.com>
2023-03-08 20:19:27 +0100

    login & register ✅

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import TopBar from './components/TopBar/TopBar';
-import Home from './pages/homepage/Home';
-import Settings from './pages/settings/Settings';
-import Single from './pages/single/Single';
-import Write from './pages/write/Write';
+import Login from './pages/login/Login';
+import Register from './pages/register/Register';
 
 function App() {
   return (
     <>
       <TopBar />
-      <Settings />
+      <Register />
     </>
   );
 }
