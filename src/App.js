b643c26ca3be7944f8c24db387e45674e4592827
Malick NDIAYE <98133571+MalicknND@users.noreply.github.com>
2023-03-08 18:59:20 +0100

    add a new post

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import TopBar from './components/TopBar/TopBar';
 import Home from './pages/homepage/Home';
 import Single from './pages/single/Single';
+import Write from './pages/write/Write';
 
 function App() {
   return (
     <>
       <TopBar />
-      <Single />
+      <Write />
     </>
   );
 }
