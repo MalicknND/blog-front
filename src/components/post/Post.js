64c0580cf56ec1a2bd42e6686a2ae442dd4a525a
Malick NDIAYE <98133571+MalicknND@users.noreply.github.com>
2023-03-12 17:36:28 +0100

    displaying all posts on the homepage

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -3,15 +3,14 @@ import './post.css';
 const Post = ({ post }) => {
   return (
     <div className="post">
-      <img
-        className="postImg"
-        src="https://images.pexels.com/photos/12679999/pexels-photo-12679999.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-        alt=""
-      />
+      {post.photo && <img className="postImg" src={post.photo} alt="" />}
       <div className="postInfo">
         <div className="postCats">
-          <span className="postCat">Music</span>
+          {post.categories.map((c) => (
+            <span className="postCat">{c.name}</span>
+          ))}
         </div>
+
         <span className="postTitle">{post.title}</span>
         <hr />
         <span className="postDate">
