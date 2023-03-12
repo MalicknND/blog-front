855401e8a72b1f25c67af8bab5d7a5097409b569
Malick NDIAYE <98133571+MalicknND@users.noreply.github.com>
2023-03-12 16:19:31 +0100

    fetching data

--- src/pages/homepage/Home.js
+++ src/pages/homepage/Home.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../../components/Header/Header';
 import Posts from '../../components/posts/Posts';
 import SideBar from '../../components/Sidebar/SideBar';
 import './Home.css';
+import axios from 'axios';
 
 const Home = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await axios.get('http://localhost:5000/api/posts');
+
+      console.log(res);
+    };
+    fetchPosts();
+  }, []);
+
   return (
     <>
       <Header />
